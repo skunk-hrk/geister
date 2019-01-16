e10445d87cac0b877fa2a7e9ad4521e23cb0920f
すかんく <>
2019-01-17 00:26:14 +0900

    スクリプト名修正

---
+++ Geister/Assets/Scripts/UserDataManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserDataManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    // プレイヤーネームを設定
+    public void SetMyname()
+    {
+
+    }
+
+    // プレイヤーネームを取得
+    public string GetMyname()
+    {
+        string user_name = "name";
+        return user_name;
+    }
+
+    // 勝利数を更新
+    public void SetWinCount()
+    {
+
+    }
+
+    // 勝利数を取得
+    public int GetWinCount()
+    {
+        int win_count = 0;
+        return win_count;
+    }
+
+    // 敗北数を更新
+    public void SetLoseCount()
+    {
+
+    }
+
+    // 敗北数を取得
+    public int GetLoseCount()
+    {
+        int lose_count = 0;
+        return lose_count;
+    }
+
+}
