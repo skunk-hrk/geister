f447a0351ed8f2f81ca78fbf1dcc88b7dd7cd0e3
すかんく <>
2019-01-17 00:09:08 +0900

    トップページと各種スクリプトファイル作成

---
+++ Geister/Assets/Scripts/MyData.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyData : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    // プレイヤーメームを設定
+    public void SetMyname()
+    {
+
+    }
+
+    // プレイヤーメームを取得
+    public string GetMyname()
+    {
+        private string user_name = "name";
+        return user_name;
+    }
+
+    // プレイヤーメームを設定
+    public void SetWincCount()
+    {
+
+    }
+
+    // プレイヤーメームを取得
+    public GetWinCount()
+    {
+
+    }
+}
